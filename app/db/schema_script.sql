4cb05d5b1ae682fedd49e7004f9be38f29fb8484
mbarja <mmbarja@gmail.com>
2020-12-05 19:37:52 -0300

    formulario

---
+++ app/db/schema_script.sql
@@ -0,0 +1,17 @@
+﻿
+CREATE TABLE datos(
+    dni integer PRIMARY KEY,
+    nombre character(30) NOT NULL,
+    apellido character(30) NOT NULL,
+    fecha_nacimiento date NOT NULL,
+    direccion character(30) NOT NULL,
+    telefono character(30) NOT NULL,
+    mail character(30) NOT NULL,
+    estado_civil character(10) NOT NULL
+);
+
+  
+INSERT INTO datos (dni,nombre,apellido,fecha_nacimiento,direccion,telefono, mail, estado_civil)
+  VALUES(31662636,'Octavio', 'Gomez', '1985-08-31', 'Rondeau 873', '2804303427', 'octaviogomez07gmail.com', 'soltero');
+
+SELECT * FROM datos;
